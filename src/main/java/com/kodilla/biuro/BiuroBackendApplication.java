902d9f62d574f231fa486174ee46bc98c243d698
Piotr Dziedzic <piotr.dziedzic1729@outlook.com>
2021-04-08 04:40:05 +0200

    FirstCommit

---
+++ src/main/java/com/kodilla/biuro/BiuroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.biuro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiuroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiuroBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/biuro/BiuroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.biuro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiuroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
